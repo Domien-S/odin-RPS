dd0f4cab1e0a3a0b7dd8282db6dcc45f664a4578
Domien-S <Domien.s@outlook.be>
2023-07-30 15:45:34 +0200

    ADD player imput

--- script.js
+++ script.js
@@ -5,3 +5,6 @@ function getComputerChoice (options) {
     return arr[Math.floor(Math.random()* arr.length)];
 }
 console.log(getComputerChoice())
+
+
+let playerSelection = prompt("Pick Rock, Paper or Scissor",) 
